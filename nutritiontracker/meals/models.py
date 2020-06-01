9a0264561da4c90e2189e370da4ce25cde13bea9
elizadavis <eliza272@aim.com>
2020-06-01 10:20:36 -0400

    Update Meals Model with Decimal Fields, DateTime Field

--- nutritiontracker/meals/models.py
+++ nutritiontracker/meals/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 class Meal(models.Model):
   name = models.CharField(max_length=100)
   servings = models.DecimalField(max_digits=5, decimal_places=2)
-  calories = models.IntegerField()
-  fat = models.IntegerField(default=0)
-  protein = models.IntegerField(default=0)
-  total_carbs = models.IntegerField(default=0)
-  fiber = models.IntegerField(default=0)
-  total_sugar = models.IntegerField(default=0)
-  added_sugar = models.IntegerField(default=0)
-  sodium = models.IntegerField(default=0)
+  calories = models.DecimalField(max_digits=5, decimal_places=2)
+  fat = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+  protein = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+  total_carbs = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+  fiber = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+  total_sugar = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+  added_sugar = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+  sodium = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+  date_time = models.DateTimeField(default=datetime.now, blank=True)
   owner = models.ForeignKey(User, related_name='meals', on_delete=models.CASCADE, null=True)
